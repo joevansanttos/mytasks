91c4665e07f2dd73b4f52ec78ecee6addc97475e
joevansanttos <joevansanttos@gmail.com>
2023-12-17 21:45:40 -0300

    add TouchableOpacity

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,55 @@
 import React from 'react';
-import { Text, View , StyleSheet} from 'react-native';
+import {Platform, Text, TextInput, TouchableOpacity , View, StyleSheet, SafeAreaView} from 'react-native';
 
 export const Home = () => {
   return(
-    <View style={styles.container}>
-      <Text style={styles.title}>Olá Dev!</Text>
-    </View>
+    <SafeAreaView style={styles.safeAre}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome, Dev!</Text>
+        <TextInput style={styles.input} />
+        <TouchableOpacity activeOpacity={0.7} style={styles.button}>
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#121214',
-        justifyContent:'center',
-        alignItems: 'center'
-    },
-    title: {
-      color: '#f1f1f1'
-    }
+  safeAre: {
+    flex: 1,
+    backgroundColor: '#121214',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#121214',
+    paddingHorizontal: 30,
+    paddingVertical: 50
+  },
+  title: {
+      color: '#f1f1f1',
+      fontSize: 24,
+      fontWeight: 'bold'
+  },
+  input:{
+    backgroundColor: '#29292e',
+    color: '#f1f1f1',
+    fontSize: 18,
+    padding: Platform.OS == 'ios' ? 15 : 12,
+    marginTop: 30,
+    borderRadius: 7,
+
+  },
+  button:{
+    backgroundColor: '#eba417',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#121214',
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
 });
