c24e5e0c6c0d3c8242115c475ccc113b76cc63e6
joevansanttos <joevansanttos@gmail.com>
2023-12-18 11:54:28 -0300

    add titleTasks

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,10 +6,12 @@ export const Home = () => {
     <SafeAreaView style={styles.safeAre}>
       <View style={styles.container}>
         <Text style={styles.title}>Welcome, Dev!</Text>
-        <TextInput style={styles.input} />
+        <TextInput placeholderTextColor="#555" placeholder='Nova tarefa...' style={styles.input} />
         <TouchableOpacity activeOpacity={0.7} style={styles.button}>
           <Text style={styles.buttonText}>Adicionar</Text>
         </TouchableOpacity>
+
+        <Text style={styles.titleTasks}>Minhas Tarefas</Text>
       </View>
     </SafeAreaView>
   );
@@ -30,6 +32,12 @@ const styles = StyleSheet.create({
       color: '#f1f1f1',
       fontSize: 24,
       fontWeight: 'bold'
+  },
+  titleTasks: {
+    color: '#f1f1f1',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 50,
   },
   input:{
     backgroundColor: '#29292e',
