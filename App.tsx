f1414e25a36483c187a2ef61e0436dfa5c2b52fd
joevansanttos <joevansanttos@gmail.com>
2023-12-20 13:25:07 -0300

    add taskscontexts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Home } from './src/pages/Home';
+import { TasksContext } from './src/context/TasksContext';
 
 const App = () => {
   return(
-    <Home></Home>
+    <TasksContext.Provider value={{id: '1', title: 'Task 01'}}>
+      <Home></Home>
+    </TasksContext.Provider>
   );
 };
 
---
+++ src/context/TasksContext.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export interface ITasksContext{
+    id: string;
+    title: string;
+}
+
+export const TasksContext = React.createContext<ITasksContext>({} as ITasksContext);
